cac3762696e0659a5f1bd2d6aa0b90e9433ca55b
Arman Akhoundy <64736713+ArmanX5@users.noreply.github.com>
2023-12-05 14:13:02 +0330

    Delete app directory

--- app/pages/phase_2.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("")
-import streamlit as st
-from src import utils
-
-st.title("Expression Page")
-st.subheader("Phase 2")
-st.divider()
-
--- app/pages/phase_3.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("")
-import streamlit as st
-from src import utils
-
-st.title("Degree Page")
-st.subheader("Phase 3")
-st.divider()
-
